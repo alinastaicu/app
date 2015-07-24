bfced6a1b9ce788757f3469d995b0853ed104cbc
Alina Staicu <alina.straicu4@gmail.com>
2015-07-24 20:07:02 +0200

    html and js

--- src/static/main.js
+++ src/static/main.js
@@ -1,1 +1,9 @@
-
+var gagagoanta = document.querySelector('#rent_checkbox');
+var iubiroanta = document.querySelector('#rent_options');
+gagagoanta.addEventListener("change", function() {
+  if(gagagoanta.checked){
+    iubiroanta.classList.remove("hide");
+  }else{
+    iubiroanta.classList.add("hide");
+  }
+});
