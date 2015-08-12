4c66d8388c8740c4660bff08280a51d13b7f3f45
Liviu Ignat <liviu@ignat.email>
2015-08-12 20:27:26 +0200

    excersises

--- src/static/main.js
+++ src/static/main.js
@@ -1,3 +1,12 @@
 $(function () {
- var primary = $('.col-sm-5').find('button[class*="btn-primary"]');
+ $('#decapotabila_div').hide();
+ $('#auto').change(function(){
+   var auto = $('#auto').val();
+   if (auto == 'porse'){
+     $('#decapotabila_div').show();
+   } else {
+    $('#decapotabila_div').hide();
+   }
+ })
+
 });
