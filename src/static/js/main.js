3edd5687d7070d695351b62291ba6edd2feabad2
Liviu Ignat <liviu@ignat.email>
2015-08-27 15:52:01 +0200

    initial setup

---
+++ src/static/js/main.js
@@ -0,0 +1,14 @@
+/*
+  Vreau sa definesti mai jos mai multe functii care sa faca operatii cu array-uri.
+  Array-urile au doar numere in ele, poti sa alegi tu ce numere sa aiba, dar sa aiba minim 4 elemente
+  Vreau mai multe seturi de teste pentru fiecare in parte
+
+  Functiile sunt urmatoarele:
+
+  - arraySuma(array) - imi aduna toate elementele din array si intoarce suma
+    eg: [1, 3, 5, 7] returns 16
+  - arrayLaPuterea2(array) = imi intoarce valoarea elementului array-ului la puterea a 2-a
+     eg: [1, 2, 3, 4] returns [1, 4, 9, 16]
+  - arrayInvers(array) - imi intoarce elementele din array in ordine inversa
+    eg: [1, 2, 3, 4] returns [4, 3, 2, 1]
+*/
---
+++ src/static/js_tests/main.spec.js
@@ -0,0 +1,5 @@
+describe("cand codul zice ca e adevarat", function() {
+  it("ar trebui sa fie adevarat", function() {
+    expect(true).toBe(true);
+  });
+});
