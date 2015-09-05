4ace2ca898572b83a9d9ea30490bb225332fc5a7
Liviu Ignat <liviu@ignat.email>
2015-09-05 12:13:59 +0200

    blah blah

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -12,3 +12,78 @@
   - arrayInvers(array) - imi intoarce elementele din array in ordine inversa
     eg: [1, 2, 3, 4] returns [4, 3, 2, 1]
 */
+
+
+function arraySuma(array){
+  var suma = 0;
+  for (var i = 0; i < array.length; i++) {
+    suma = array[i] + suma
+    }
+    return suma;
+  }
+
+
+function arrayLaPuterea2(array){
+  var puterea = [];
+  for (var i = 0; i < array.length; i++) {
+    puterea [i]= Math.pow(array[i],2);
+  }
+  return puterea;
+}
+
+function arrayInvers(array){
+  var invers = [];
+  var index = 0;
+
+  for (var i = array.length - 1; i >= 0; i--) {
+    invers[index] = array[i];
+    index = index + 1;
+  }
+
+  return invers;
+}
+
+function secventaNr(max, vreauPare) {
+  var par = [];
+
+  var i = 2;
+
+  if(!vreauPare){
+
+    i = 1;
+  }
+
+  for (; i <= max ; i = i + 2) {
+    par.push(i);
+  }
+  return par;
+}
+
+function secventaNrPare(max){
+  return secventaNr(max,true);
+}
+
+function secventaNrImpare(max) {
+  return secventaNr(max,false);
+}
+
+function secventaNrPareInversa(max){
+  var array = secventaNrPare(max);
+  var arrayInv = arrayInvers(array);
+  return arrayInv;
+
+}
+
+function secventaNrImpareInversa(max){
+  var array = secventaNrImpare(max);
+  var arrayImp = arrayInvers(array);
+  return arrayImp;
+
+}
+
+function sumaPatratelorNumerelorPare(max){
+  var NrPar = secventaNrPare(max);
+  var Putere = arrayLaPuterea2(NrPar) ;
+  var Sum = arraySuma(Putere);
+  return Sum;
+}
--- src/static/js_tests/main.spec.js
+++ src/static/js_tests/main.spec.js
@@ -1,5 +1,80 @@
-describe("cand codul zice ca e adevarat", function() {
-  it("ar trebui sa fie adevarat", function() {
+describe('Adevarat este adevarat', function() {
+  it('should be true', function() {
+    expect(true).toEqual(true);
     expect(true).toBe(true);
   });
 });
+
+describe('cand array este [1, 3, 5, 7]', function() {
+  it('array suma intoarce 16', function() {
+    expect(arraySuma([1, 3, 5, 7])).toBe(16);
+  });
+});
+
+
+describe('cand array este [1, 2, 3, 4]', function() {
+  it('array la putere a doua intoarce[1, 4, 9, 16]', function() {
+    expect(arrayLaPuterea2([1, 2, 3, 4])).toEqual([1, 4, 9, 16]);
+  });
+});
+
+describe('cand array este [1, 2, 3, 4]', function() {
+  it('intoarce elementele din array in ordine inversa [1, 2, 3, 4}', function() {
+    expect(arrayInvers([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
+  });
+});
+
+describe('cand array este [1, 2, 3, 4]', function() {
+  it('intoarce elementele din array in ordine inversa [1, 2, 3, 4}', function() {
+    expect(arrayInvers([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
+  });
+});
+
+
+describe('secventa nr pare: max 5', function() {
+  it('intoarce [2, 4]', function() {
+    expect(secventaNrPare(5)).toEqual([2, 4]);
+  });
+});
+
+describe('secventa nr pare: max 10', function() {
+  it('intoarce [2, 4, 6, 8, 10]', function() {
+    expect(secventaNrPare(10)).toEqual([2, 4, 6, 8 , 10]);
+  });
+});
+
+
+describe('secventa nr impare: max 5', function() {
+  it('intoarce [1, 3, 5]', function() {
+    expect(secventaNrImpare(5)).toEqual([1, 3, 5]);
+  });
+});
+
+describe('secventa nr impare: max 10', function() {
+  it('intoarce [1, 3, 5, 7, 9]', function() {
+    expect(secventaNrImpare(10)).toEqual([1, 3, 5, 7, 9]);
+  });
+});
+
+
+describe('secventa nr pare in ordine inversa max = 5', function() {
+  it('intoarce [4, 2]', function() {
+    expect(secventaNrPareInversa(5)).toEqual([4, 2]);
+  });
+});
+
+
+describe('secventa inversa de nr impare: max 10', function() {
+  it('intoarce [9, 7, 5, 3, 1]', function() {
+    expect(secventaNrImpareInversa(10)).toEqual([9, 7, 5, 3, 1]);
+  });
+});
+
+
+describe('secventa inversa de nr impare: max 5', function() {
+  it('intoarce 20', function() {
+    expect(sumaPatratelorNumerelorPare(5)).toEqual(20);
+  });
+});
+
+
