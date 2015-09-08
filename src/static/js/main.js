161029083a6da1f38494c9d70c89d118d9047aa7
Liviu Ignat <liviu@ignat.email>
2015-09-08 14:13:41 +0200

    objs

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,13 +1,58 @@
-var player1 ={
-  name:"Liviu"
-};
-
-var player2 = {
-  name:"Alina"
-};
-
-var table = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
-
-function playerWon(sign) {
-  return true;
+function getCurrentDate() {
+  return new Date();
 }
+
+var Person = function () {
+  this.name = '';
+  this.firstName = '';
+  this.birthDate = new Date();
+  this.sex = '';
+  this.address = new Address();
+
+  this.fullName = function () {
+    return this.name + ', ' + this.firstName;
+  }
+
+  this.age = function () {
+    var currentDate = getCurrentDate();
+    var difference = currentDate.getTime() - this.birthDate.getTime();
+
+    return Math.floor(difference / (1000 * 60 * 60 * 24 * 365.25));
+  }
+};
+
+var Address = function () {
+  this.streetName = '';
+  this.streetNumber = '';
+  this.postalCode = '';
+};
+
+var BankAccount = function (person) {
+  if(person.age() < 18) {
+    throw new Error('Not allowed to have an account at 18!');
+  }
+
+  this.iban = '';
+  this.amount = 0;
+  this.person = person;
+
+  this.addAmount = function (sum) {
+    this.amount = this.amount + sum;
+  };
+}
+
+function transferAmount(accountFrom, accountTo, sum) {
+  if(accountFrom.amount - sum < 0) {
+    throw new Error('Isufficient founds!');
+  }
+
+  accountTo.amount = accountTo.amount + sum;
+  accountFrom.amount = accountFrom.amount - sum;
+}
+
+// var liviusAccount = new BankAccount(liviu);
+// liviusAccount.amount = 30000.00;
+// var alinasAccount = new BankAccount(alina);
+// alinasAccount.amount = 2500.54;
+
+
--- src/static/js_tests/main.spec.js
+++ src/static/js_tests/main.spec.js
@@ -1,36 +1,144 @@
-describe("Initial state", function() {
-  it("should have player1 defined", function() {
-    expect(player1).toBeDefined();
-  });
+describe('When Alina and Liviu are created', function() {
+  var alina, liviu;
 
-  it("should have player1 Liviu", function() {
-    expect(player1.name).toBe('Liviu');
-  });
-
-  it("shoud have player2 defined", function() {
-    expect(player2).toBeDefined();
-  });
-
-  it("should have player2 Alina", function() {
-    expect(player2.name).toBe("Alina");
-  });
-  it("should have table whidth 3", function() {
-    expect(table.length).toBe(3);
-  });
-
-  it("player with x should not have won", function() {
-    expect(playerWon('x')).toBe(false);
-  });
-});
-
-describe("when player did first line", function() {
   beforeEach(function() {
-    table[0][0] = 'x';
-    table[0][1] = 'x';
-    table[0][2] = 'x';
+    spyOn(window, 'getCurrentDate').and.returnValue(new Date(2015, 9, 25))
   });
 
-  it("player with x should have won", function() {
-    expect(playerWon('x')).toBe(true);
+  beforeEach(function() {
+    alina = new Person();
+    alina.name = 'Alina';
+    alina.firstName = 'Staicu';
+    alina.birthDate = new Date(1984, 11, 4);
+    alina.sex = 'female';
+    alina.address = new Address();
+    alina.address.streetName = 'Nymphemburgerstr.';
+    alina.address.streetNumber = '126';
+    alina.address.postalCode = '80636';
   });
+
+  beforeEach(function() {
+    liviu = new Person();
+    liviu.name = 'Liviu';
+    liviu.firstName = 'Ignat';
+    liviu.birthDate = new Date(1983, 9, 26);
+    liviu.sex = 'male';
+    liviu.address = new Address();
+    liviu.address.streetName = 'Nymphemburgerstr.';
+    liviu.address.streetNumber = '126';
+    liviu.address.postalCode = '80636';
+  });
+
+  it('Alina should have correct age', function() {
+    expect(alina.age()).toBe(30);
+  });
+
+  it('Liviu should have correct age', function() {
+    expect(liviu.age()).toBe(31);
+  });
+
+  it('Liviu\'s fullName schould be Liviu, Ignat', function() {
+    expect(liviu.fullName()).toBe('Liviu, Ignat');
+  });
+
+
+  describe('When BankAccount is created for Alina', function() {
+
+    var alinasAccount;
+    beforeEach(function() {
+      alinasAccount = new BankAccount(alina);
+    });
+
+    describe('When person is under 18', function() {
+      beforeEach(function() {
+        var today = new Date(getCurrentDate().getTime());
+        today.setFullYear(today.getFullYear() - 10);
+
+        alina.birthDate = today;
+      });
+
+      it('Should fail creating the account', function() {
+        var func = function() {
+          new BankAccount(alina);
+        };
+
+        expect(func).toThrow(new Error('Not allowed to have an account at 18!'));
+      });
+    });
+
+    describe('When person is older than 18', function() {
+      var account;
+
+      beforeEach(function() {
+        var today = new Date(getCurrentDate().getTime());
+        today.setFullYear(today.getFullYear() - 20);
+
+        alina.birthDate = today;
+        account = new BankAccount(alina);
+      });
+
+      it('Should create the bank account', function() {
+        expect(account).toBeDefined();
+      });
+
+      it('Account should have a 0 amount', function() {
+        expect(account.amount).toBe(0);
+      });
+    });
+
+    describe('When BankAccount is created for liviu', function() {
+      var liviusAccount;
+      beforeEach(function() {
+        liviusAccount = new BankAccount(liviu);
+      });
+
+      describe('When transfering 100 from alina to livius bank account', function() {
+        it('should throw an error Isufficient founds', function() {
+          var func = function() {
+            transferAmount(alinasAccount, liviusAccount, 100);
+          };
+
+          expect(func).toThrow(new Error('Isufficient founds!'));
+        });
+      });
+
+      describe('When alina adds 1300 to her account', function() {
+        beforeEach(function() {
+          alinasAccount.addAmount(1300);
+        });
+
+        it('Alina should have a balance of 1300', function() {
+          expect(alinasAccount.amount).toBe(1300);
+        });
+
+        describe('When alina adds 300.15 to her account', function() {
+          beforeEach(function() {
+            alinasAccount.addAmount(300.15);
+          });
+
+          it('Alina should have a balance of 1600.15', function() {
+            expect(alinasAccount.amount).toBe(1600.15);
+          });
+        });
+
+        describe('When alina transfers 150 to livius bank account', function() {
+          beforeEach(function() {
+            transferAmount(alinasAccount, liviusAccount, 150);
+          });
+
+          it('Alina should have 1150 balance', function() {
+            expect(alinasAccount.amount).toBe(1150);
+          });
+
+          it('Liviu should have 150 balance', function() {
+            expect(liviusAccount.amount).toBe(150);
+          });
+        });
+      });
+    });
+  });
+
+
 });
+
+
